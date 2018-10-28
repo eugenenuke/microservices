a1f49ce0e0951dc13903daf75abeaef9bfe2c9e5
Evgeniy Yanyuk <eugenenuke@gmail.com>
2018-10-28 15:04:58 +0300

    add prometheus for monitoring

---
+++ prometheus/prometheus-host.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+docker-machine create --driver google \
+	--google-project docker-220019 \
+	--google-machine-image https://www.googleapis.com/compute/v1/projects/ubuntu-os-cloud/global/images/family/ubuntu-1604-lts \
+	--google-machine-type n1-standard-1 \
+	--google-zone europe-west1-b \
+	vm1
